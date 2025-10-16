cfa355c94fdd65f58b71b77af306c5949995e5b4
ndias4 <fdn.dias10@gmail.com>
2025-10-16 00:21:38 -0300

    fix(db): habilita SSL para conexão com o banco de dados

--- src/config/db.mjs
+++ src/config/db.mjs
@@ -1,12 +1,9 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 
-// Verificamos se estamos no ambiente de teste (definido pelo comando 'npm test')
 if (process.env.NODE_ENV === 'test') {
-    // Se sim, carregamos as variáveis do banco de TESTE
     dotenv.config({ path: '.env.test' });
 } else {
-    // Senão, carregamos as variáveis do banco de DESENVOLVIMENTO
     dotenv.config();
 }
 
@@ -16,6 +13,10 @@ export const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE, 
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
+  // ADICIONE ESTA LINHA PARA ATIVAR O SSL
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
